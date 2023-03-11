afee529c02a6ee6e90a1a6edf7ae71d17ce37c22
Sagar Sane <sagar2217@gmail.com>
2023-03-11 11:44:36 -0500

    Done until basic right content. RECOIL next

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,19 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import AuthButtons from "./AuthButtons";
+
+type RightContentProps = {
+  //user : any
+};
+
+const RightContent: React.FC<RightContentProps> = () => {
+  return (
+      <>
+          
+      <Flex justify="center">
+        <AuthButtons />
+      </Flex>
+    </>
+  );
+};
+export default RightContent;
